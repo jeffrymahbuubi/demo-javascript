71b3f2908950de884350fdbff944c5c6edda1ed8
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-01-15 01:39:55 +0000

    fix: prettier

--- currency-converter/index.js
+++ currency-converter/index.js
@@ -1,40 +1,39 @@
-const idr = 1_000_000
+const idr = 1_000_000;
 
 const dataCurrency = [
-    {
-        name: "$NTD",
-        divisor: 506
-    },
-    {
-        name: "$USD",
-        divisor: 15114
-    },
-    {
-        name: "¥JPY",
-        divisor: 118
-    },
-    {
-        name: "¥CNY",
-        divisor: 2254
-    },
-    {
-        name: "€Euro",
-        divisor: 16389
-    },
-    {
-        name: "£Pound sterling",
-        divisor: 18486
-    }
-]
+  {
+    name: "$NTD",
+    divisor: 506,
+  },
+  {
+    name: "$USD",
+    divisor: 15114,
+  },
+  {
+    name: "¥JPY",
+    divisor: 118,
+  },
+  {
+    name: "¥CNY",
+    divisor: 2254,
+  },
+  {
+    name: "€Euro",
+    divisor: 16389,
+  },
+  {
+    name: "£Pound sterling",
+    divisor: 18486,
+  },
+];
 
 for (let index = 0; index < dataCurrency.length; index = index + 1) {
-    const currency = dataCurrency[index];
-    const newCurrency = {
-        name: `${currency.name}`,
-        converted: idr / currency.divisor
-    }
-    console.log(`IDR ${idr} is equal to ${newCurrency.name} ${newCurrency.converted}`)
+  const currency = dataCurrency[index];
+  const newCurrency = {
+    name: `${currency.name}`,
+    converted: idr / currency.divisor,
+  };
+  console.log(
+    `IDR ${idr} is equal to ${newCurrency.name} ${newCurrency.converted}`
+  );
 }
-
-
-
