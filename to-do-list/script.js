8108531cc0d3226abbd436b723c2336dcd576b40
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-01 12:50:24 +0000

    refractor: variable name and change div to form

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -1,58 +1,74 @@
 // Declare Variable
 
-const listToDo = document.getElementById("list-todo");
-const input = document.getElementById("user-input");
-const addBtn = document.getElementById("add");
+const listTodo = document.getElementById("list-todo");
+const userInput = document.getElementById("user-input");
+const addButton = document.getElementById("add");
 
 //Function to check input
-function inputLength() {
-  return input.value.length;
+function checkInputLength() {
+  return userInput.value.length;
 }
 
 // Function to add todo-list
-function createListElement() {
+function createTodoItemElement() {
   // Create variable for adding list and delete button
-  const li = document.createElement("li");
+  const todoItem = document.createElement("li");
   const deleteBtn = createDeleteButton();
+
   // Add list and delete button at the same time
-  li.appendChild(document.createTextNode(input.value));
-  li.appendChild(deleteBtn);
-  listToDo.appendChild(li);
+  todoItem.appendChild(document.createTextNode(userInput.value));
+  todoItem.appendChild(deleteBtn);
+  listTodo.appendChild(todoItem);
+
   // Delete list if delete button clicked
   deleteBtn.addEventListener("click", () => {
-    li.remove();
+    todoItem.remove();
   });
+
   // Add strikethrough if list clicked
-  li.addEventListener("click", () => {
-    li.classList.toggle("done");
+  todoItem.addEventListener("click", () => {
+    todoItem.classList.toggle("done");
   });
 }
 
 function createDeleteButton() {
-  const deleteBtn = document.createElement("button");
-  deleteBtn.setAttribute("title", "Delete List!");
-  deleteBtn.classList.add("delete-button");
-  deleteBtn.innerHTML = `<i class="fa-sharp fa-solid fa-trash"></i>`;
-  return deleteBtn;
+  const deleteButton = document.createElement("button");
+  deleteButton.setAttribute("title", "Delete List!");
+  deleteButton.classList.add("delete-button");
+  deleteButton.innerHTML = `<i class="fa-sharp fa-solid fa-trash"></i>`;
+  return deleteButton;
 }
 
-// Function to add todo-list after clicking add-button
-function createListAfterClick() {
-  if (inputLength() > 0) {
-    createListElement();
-  }
-}
-
-// Function to add todo-list after pressing 'Enter' button
-function addListAfterKeypress(event) {
-  if (inputLength() > 0 && event.which == 13) {
-    createListElement();
+// Function to add todo-item after clicking add-button
+function createTodoItemAfterCheckLength() {
+  if (checkInputLength() > 0) {
+    createTodoItemElement();
   }
 }
 
 function runApp() {
-  addBtn.addEventListener("click", createListAfterClick);
-  input.addEventListener("keypress", addListAfterKeypress);
+  addButton.addEventListener("click", createTodoItemAfterCheckLength);
+  userInput.addEventListener("submit", createTodoItemAfterCheckLength);
 }
 
 runApp();
+
+// const listTodo
+// const userInput
+// const addButton
+// function checkInputLength()
+// function createTodoItemElement()
+// Todo List => Todo Item
+// Todos => Todo
+// li to const todo
+// const deleteButton
+// Pisah dengan enter
+// div dibungkus dalam form
+// Bagaimana jika semua todosnya di deklrasi diatas
+//  const dataTodos = [
+//   {
+//     id: "1"
+//     text:'Programming'
+//     done (class): true or false
+//   }
+// ]
