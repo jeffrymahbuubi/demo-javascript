9e38e3310b858d8835e280d236fadda5d2f4f170
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-01 11:00:01 +0000

    feat: update complex-currency

--- complex-currency-converter/currency-api/script.js
+++ complex-currency-converter/currency-api/script.js
@@ -1,1 +1,28 @@
-console.log("test");
+// Link Country By Currency Name
+urlCurrencyCode =
+  "https://country-api-jeffrymahbuubi.netlify.app/data/src/country-by-currency-code.json";
+
+// Link Country By Currency Code
+urlCurrencyName =
+  "https://country-api-jeffrymahbuubi.netlify.app/data/src/country-by-currency-name.json";
+
+// Link Convert Currency API Free
+urlConvertCurency =
+  "var requestURL = 'https://api.exchangerate.host/convert?from=USD&to=EUR';";
+
+const fromCurrency = document.getElementById("from-currency");
+const toCurrency = document.getElementById("to-currency");
+
+// Get Country Names From API
+async function getCountriesName() {
+  const apiURL = urlCurrencyCode;
+  try {
+    const response = await fetch(apiURL);
+    apiCountries = await response.json();
+    console.log(apiCountries);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+getCountriesName();
