4f9e2ac99b29487c72aca264a3b96342310f888a
Aunuun Jeffry Mahbuubi <jeffryaun@gmail.com>
2023-02-02 13:20:04 +0000

    feat: complex-currency update project

--- complex-currency-converter/script.js
+++ complex-currency-converter/script.js
@@ -9,19 +9,43 @@ urlConvertCurency =
 const fromCurrency = document.getElementById("from-currency");
 const toCurrency = document.getElementById("to-currency");
 
-// Get Country Names From API
-async function getCountriesName() {
-  const proxyUrl = "https://proxy-api-jeffrymahbuubi.netlify.app/";
-  const apiUrl =
-    "https://country-api-jeffrymahbuubi.netlify.app/data/src/country-by-currency-code.json";
-  try {
-    const response = await fetch(apiUrl, {
-      method: "GET",
-      mode: "no-cors",
-    });
-    const apiCurrencies = await response.json();
-    console.log(apiCurrencies);
-  } catch (error) {}
+let countriesName = [];
+let countriesCurrency = [];
+
+function showCountryAndCode() {
+  apiCountry.forEach((item) => {
+    countriesName.push(item.country);
+    countriesCurrency.push(item.currency_code);
+  });
+
+  // Create element option and pass it to "from-country" and "to-country"
+  countriesName.forEach((countryName) => {
+    const optionElement = document.createElement("option");
+    optionElement.innerHTML = countryName;
+    fromCurrency.append(optionElement);
+    toCurrency.append(optionElement);
+  });
 }
 
-getCountriesName();
+// The reason we use let instead a constant, because in the beginning we're setting it as
+// an empty array, but he're we actually changing the value of it to pass in the quote.
+let apiCountry = [];
+
+// Get Countries Name and Currency Codes Names From API
+async function getCountryInformations() {
+  const apiUrl =
+    "https://jeffrymahbuubi.github.io/currency-api/data/src/country-by-currency-code.json";
+  try {
+    // const response will not be populated until it has some data fetched from the API
+    // Only setup the const response when we get the data
+    const response = await fetch(apiUrl);
+
+    // apiCurrencies turning the data fetched into a JSON object from a Web Serer
+    apiCountry = await response.json();
+    showCountryAndCode();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getCountryInformations();
